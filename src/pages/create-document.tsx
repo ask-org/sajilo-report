17eee4d1240f95a3420a7cb47c09c57a77847a50
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-10-02 19:18:03 +0545

    Base started for the editor and preview section

--- src/pages/create-document.tsx
+++ src/pages/create-document.tsx
@@ -1,13 +1,17 @@
+// Implementation Detail: This will divide the screen into two parts, one for the editor and one for the preview.
+// The editor part will contain the sections field which will have title and a paragraph field. the user can choose to add 'sub section' to the section which will again provide him with  a title and content.
+
 import { useState } from "react";
-import StartingModel from "../components/create-documents/model/startingModel";
+
+type Tsection = {
+  heading: string;
+  sectionId: string;
+  content: string;
+  subSections?: Tsection[];
+};
 
 export default function CreateDocument() {
-  const [openModel, setOpenModel] = useState(true);
+  const [section, setSection] = useState<Tsection[]>([]);
 
-  return (
-    <div>
-      <h1 className="bg-red-300">Create Document</h1>
-      {openModel && <StartingModel setOpenModel={setOpenModel} />}
-    </div>
-  );
+  return <div></div>;
 }
