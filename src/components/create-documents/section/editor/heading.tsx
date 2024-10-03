128184951a8e0a5298567a054ef0789d15e46758
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-10-03 11:24:08 +0545

    heading works properly

--- src/components/create-documents/section/editor/heading.tsx
+++ src/components/create-documents/section/editor/heading.tsx
@@ -2,18 +2,22 @@ import React from "react";
 
 interface HeadingProps {
   heading: string;
-  setHeading: (value: string) => void;
+  setHeading: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Heading: React.FC<HeadingProps> = ({ heading, setHeading }) => {
   return (
     <div>
       <label htmlFor="heading-input">Heading:</label>
+      &nbsp;
       <input
         id="heading-input"
         type="text"
         value={heading}
-        onChange={(e) => setHeading(e.target.value)}
+        className="text-black"
+        onChange={(e) => {
+          setHeading(e.target.value);
+        }}
         aria-label="Heading:"
       />
     </div>
