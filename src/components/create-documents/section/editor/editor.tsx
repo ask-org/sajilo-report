56929caa51e8e49b45a96b07f5b26ab9889f57c6
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-10-03 11:00:38 +0545

    mend
    Heading tests also check for the state

--- src/components/create-documents/section/editor/editor.tsx
+++ src/components/create-documents/section/editor/editor.tsx
@@ -1,7 +1,29 @@
-const Editor = () => {
+import { useEffect, useState } from "react";
+import { Tsection } from "../../../../types/create-document";
+
+type EditorProps = {
+  section: Tsection[];
+  setSection: React.Dispatch<React.SetStateAction<Tsection[]>>;
+};
+const Editor = ({ section, setSection }: EditorProps) => {
+  // TODO: this feature is not implemented yet, and will come in play when we implement the template
+  const [data, setData] = useState<Tsection[]>(section);
+
+  useEffect(() => {
+    if (data !== section) {
+      setData(section);
+    }
+  }, [section, data]);
+
+  // const onSubmit = () => {
+  //   setSection(data);
+  // };
+
   return (
     <div>
-      <h1>Editor</h1>
+      {data.map((section, index) => {
+        return <div> individual section </div>;
+      })}
     </div>
   );
 };
--- src/pages/create-document.tsx
+++ src/pages/create-document.tsx
@@ -1,17 +1,11 @@
 // Implementation Detail: This will divide the screen into two parts, one for the editor and one for the preview.
 // The editor part will contain the sections field which will have title and a paragraph field. the user can choose to add 'sub section' to the section which will again provide him with  a title and content.
 
-import { useState } from "react";
-
-type Tsection = {
-  heading: string;
-  sectionId: string;
-  content: string;
-  subSections?: Tsection[];
-};
+// import { useState } from "react";
+// import { Tsection } from "../components/types/create-document";
 
 export default function CreateDocument() {
-  const [section, setSection] = useState<Tsection[]>([]);
+  // const [section, setSection] = useState<Tsection[]>([]);
 
   return <div></div>;
 }
