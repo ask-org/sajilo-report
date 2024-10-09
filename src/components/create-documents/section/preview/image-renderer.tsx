bdfd16dee87af132b1da3e19b5c0ed71ebd16315
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-10-09 11:19:30 +0545

    image are now handled better
    - images show up at the center of the screen
    - if the user doesnt give any image, its shown
    - the image section on the editor has a better ui

--- src/components/create-documents/section/preview/image-renderer.tsx
+++ src/components/create-documents/section/preview/image-renderer.tsx
@@ -2,15 +2,26 @@ import { TFigure } from "../../../../types/create-document";
 
 const ImageRenderer = ({ figures }: { figures: TFigure[] }) => {
   return (
-    <div className="text-center">
+    <div className="text-center flex flex-col gap-2">
       {figures.map((figure, index) => {
         return (
           <figure key={index}>
-            <img
-              src={URL.createObjectURL(figure.src)}
-              alt={`figure ${index}`}
-            />
-            <figcaption>{figure.caption}</figcaption>
+            {figure.src ? (
+              <>
+                <img
+                  src={URL.createObjectURL(figure.src)}
+                  alt={figure.caption}
+                  className="mx-auto"
+                />
+                <figcaption className="text-sm text-gray-500">
+                  {figure.caption}
+                </figcaption>
+              </>
+            ) : (
+              <div className="bg-gray-200 h-40 w-80 flex justify-center items-center">
+                Empty image detected
+              </div>
+            )}
           </figure>
         );
       })}
