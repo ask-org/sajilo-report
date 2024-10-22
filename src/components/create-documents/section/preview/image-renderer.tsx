30c179ff2c7c56516bd393583726f28c9cc1772a
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-10-22 18:08:27 +0545

    Feat : image renders

--- src/components/create-documents/section/preview/image-renderer.tsx
+++ src/components/create-documents/section/preview/image-renderer.tsx
@@ -1,30 +1,22 @@
 import { TFigure } from "../../../../types/create-document";
 
-const ImageRenderer = ({ figures }: { figures: TFigure[] }) => {
+const ImageRenderer = ({ figure }: { figure: TFigure }) => {
   return (
     <div className="text-center flex flex-col gap-2">
-      {figures.map((figure, index) => {
-        return (
-          <figure key={index}>
-            {figure.src ? (
-              <>
-                <img
-                  src={URL.createObjectURL(figure.src)}
-                  alt={figure.caption}
-                  className="mx-auto"
-                />
-                <figcaption className="text-sm text-gray-500">
-                  {figure.caption}
-                </figcaption>
-              </>
-            ) : (
-              <div className="bg-gray-200 h-40 w-80 flex justify-center items-center">
-                Empty image detected
-              </div>
-            )}
-          </figure>
-        );
-      })}
+      <figure>
+        {figure.src ? (
+          <>
+            <img src={figure.src} alt={figure.caption} className="mx-auto" />
+            <figcaption className="text-sm text-gray-500">
+              {figure.caption}
+            </figcaption>
+          </>
+        ) : (
+          <div className="bg-gray-200 h-40 w-80 flex justify-center items-center">
+            Empty image detected
+          </div>
+        )}
+      </figure>
     </div>
   );
 };
